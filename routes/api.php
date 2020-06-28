8718fc368bd604bc0bbf3b1356aa7ca6b9d83409
Lukas Kalkhoran <lukasersej@gmail.com>
2020-06-28 19:57:56 +0200

    Fix incorrect routes from planning

--- routes/api.php
+++ routes/api.php
@@ -14,39 +14,39 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['middleware' => 'auth:api'], function () {
-    Route::post('logout', 'Auth\LoginController@logout');
+  Route::post('logout', 'Auth\LoginController@logout');
 
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+  Route::get('/user', function (Request $request) {
+    return $request->user();
+  });
 
-    Route::patch('settings/profile', 'Settings\ProfileController@update');
-    Route::patch('settings/password', 'Settings\PasswordController@update');
+  Route::patch('settings/profile', 'Settings\ProfileController@update');
+  Route::patch('settings/password', 'Settings\PasswordController@update');
 
-    // Recipe Routes
-    Route::post('/recipe', 'RecipeController@create');
-    Route::put('/recipe', 'RecipeController@update');
-    Route::delete('/recipe', 'RecipeController@delete');
-    Route::post('/recipe/private', 'RecipeController@private');
-    Route::post('/recipe/private', 'FavoriteController@create');
-    Route::get('/recipe/private', 'RecipeController@index');
+  // Recipe Routes
+  Route::post('/recipe', 'RecipeController@store');
+  Route::put('/recipe/{id}', 'RecipeController@update');
+  Route::delete('/recipe/{id}', 'RecipeController@destroy');
+  Route::post('/recipe/private', 'RecipeController@private');
+//    Route::post('/recipe/private', 'FavoriteController@create');
+  Route::get('/recipe/private', 'RecipeController@index');
 });
 
 Route::group(['middleware' => 'guest:api'], function () {
-    Route::post('login', 'Auth\LoginController@login');
-    Route::post('register', 'Auth\RegisterController@register');
+  Route::post('login', 'Auth\LoginController@login');
+  Route::post('register', 'Auth\RegisterController@register');
 
-    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
-    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+  Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+  Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
-    Route::post('email/verify/{user}', 'Auth\VerificationController@verify')->name('verification.verify');
-    Route::post('email/resend', 'Auth\VerificationController@resend');
+  Route::post('email/verify/{user}', 'Auth\VerificationController@verify')->name('verification.verify');
+  Route::post('email/resend', 'Auth\VerificationController@resend');
 
-    Route::post('oauth/{driver}', 'Auth\OAuthController@redirectToProvider');
-    Route::get('oauth/{driver}/callback', 'Auth\OAuthController@handleProviderCallback')->name('oauth.callback');
+  Route::post('oauth/{driver}', 'Auth\OAuthController@redirectToProvider');
+  Route::get('oauth/{driver}/callback', 'Auth\OAuthController@handleProviderCallback')->name('oauth.callback');
 
-    // Recipe Routes
-    Route::get('/recipe', 'RecipeController@index');
-    Route::get('/recipe/{recipe}', 'RecipeController@show');
-    Route::get('/search', 'RecipeController@search');
+  // Recipe Routes
+  Route::get('/recipe', 'RecipeController@index');
+  Route::get('/recipe/{recipe}', 'RecipeController@show');
+  Route::get('/search', 'RecipeController@search');
 });
