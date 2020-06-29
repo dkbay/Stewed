747bab5e5fae2880f4598fd27a4430f88953839e
Lukas Kalkhoran <lukasersej@gmail.com>
2020-06-30 01:39:23 +0200

    Add compoenents for ingredients and steps

---
+++ resources/js/components/Ingredients.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>{{ ingredients }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Ingredients',
+  props: {
+    ingredients: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  div {
+    max-width: 350px;
+    padding: 25px;
+    background-color: #F1F1F6;
+    color: #484153;
+  }
+</style>
---
+++ resources/js/components/Steps.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Sådan gør du</h1>
+    <p class="ml-3">{{ steps }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Steps',
+  props: {
+    steps: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  p {
+    width: 350px;
+    padding: 25px;
+    color: #484153;
+    border-left: dotted 2px #484153;
+  }
+</style>
