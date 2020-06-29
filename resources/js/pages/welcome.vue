83134a30e6a927989a469f3ecf0c74fcfdc2f590
Lukas Kalkhoran <lukasersej@gmail.com>
2020-06-29 22:32:57 +0200

    Revert "Add components"
    This reverts commit f93d5aae1f9c0927097249380a095883ba4b82ba.

--- resources/js/pages/welcome.vue
+++ resources/js/pages/welcome.vue
@@ -1,47 +1,61 @@
 <template>
   <div>
+    <div class="top-right links">
+      <template v-if="authenticated">
+        <router-link :to="{ name: 'home' }">
+          {{ $t('home') }}
+        </router-link>
+      </template>
+      <template v-else>
+        <router-link :to="{ name: 'login' }">
+          {{ $t('login') }}
+        </router-link>
+        <router-link :to="{ name: 'register' }">
+          {{ $t('register') }}
+        </router-link>
+      </template>
+    </div>
+
     <div class="text-center">
       <div class="title mb-4">
         {{ title }}
       </div>
-      <Ingredients :ingredients="ingredients"/>
-      <Steps/>
+
+      <div class="links">
+        <a href="https://github.com/cretueusebiu/laravel-vue-spa">github.com/cretueusebiu/laravel-vue-spa</a>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  import { mapGetters } from 'vuex'
-  import Ingredients from '../components/Ingredients'
-  import Steps from '../components/Steps'
+import { mapGetters } from 'vuex'
 
-  export default {
-    components: { Navbar, Ingredients, Steps },
+export default {
+  layout: 'basic',
 
-    metaInfo () {
-      return { title: this.$t('home') }
-    },
+  metaInfo () {
+    return { title: this.$t('home') }
+  },
 
-    data: () => ({
-      title: window.config.appName,
-      ingredients: null,
-      steps: null
-    }),
+  data: () => ({
+    title: window.config.appName
+  }),
 
-    computed: mapGetters({
-      authenticated: 'auth/check'
-    })
-  }
+  computed: mapGetters({
+    authenticated: 'auth/check'
+  })
+}
 </script>
 
 <style scoped>
-  .top-right {
-    position: absolute;
-    right: 10px;
-    top: 18px;
-  }
+.top-right {
+  position: absolute;
+  right: 10px;
+  top: 18px;
+}
 
-  .title {
-    font-size: 85px;
-  }
+.title {
+  font-size: 85px;
+}
 </style>
