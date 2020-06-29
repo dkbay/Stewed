f93d5aae1f9c0927097249380a095883ba4b82ba
Dkbay <lukasersej@gmail.com>
2020-06-29 21:34:40 +0200

    Add components

---
+++ resources/js/components/Ingredients.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>{{ingredient}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Ingredients',
+    props: {
+      ingredients: String,
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Steps.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'Steps',
+    props: {
+      steps: String,
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
